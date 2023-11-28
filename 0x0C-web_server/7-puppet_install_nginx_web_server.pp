1723f1d919657e9a55e3e31c35892cbe6dffdb80
BadrRibzat <badrribzat@gmail.com>
2023-11-28 06:11:49 -0800

    again

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,25 +1,48 @@
-# File:   7-puppet_install_nginx_web_server.pp
-# Author: Alex Orland Arévalo Tribaldos
-# email:  <3915@holbertonschool.com>
+# installs nginx with a 301 and 404 page
 
-# Using Puppet| Install Nginx server, setup and configuration
-
-package { 'nginx':
-  ensure => 'installed'
+# general update before install
+exec { 'general update':
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => 'apt-get -y update',
+  user    => 'root',
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World',
+# install nginx
+exec { 'install nginx':
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => 'apt-get -y install nginx',
+  user    => 'root',
+  require => exec['general update'],
 }
 
-file_line { 'redirection-301':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+# create new default index page and 404 page
+exec { 'new index and 404 pages':
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => 'echo "Holberton School" > /var/www/html/index.html; echo "Ceci n\'est pas une page" > /var/www/html/404.html',
+  user    => 'root',
+  require => exec['install nginx'],
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+# update nginx sites-available with 301
+exec { 'update sites-available with 301':
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => 'sed -i "/listen \[::]:80 default_server;/a\	rewrite ^/redirect_me/?.*$ https://www.youtube.com/watch?v=mTEros927Ow permanent;" /etc/nginx/sites-available/default',
+  user    => 'root',
+  require => exec['new index and 404 pages'],
+}
+
+# update nginx sites-available with 404
+exec { 'update sites-available with 404':
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => 'sed -i "/listen \[::]:80 default_server;/a\	error_page 404 /404.html;" /etc/nginx/sites-available/default',
+  user    => 'root',
+  require => exec['update sites-available with 301'],
+}
+
+# start nginx
+exec { 'start nginx':
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => 'service nginx start',
+  user    => 'root',
+  require => exec['update sites-available with 404'],
 }
